3be9e7dc8019044c0a8b820b2a6cdcfb1d682c1d
Aerospace91 <beenters@gmail.com>
2025-08-06 15:06:05 -0700

    Cleaning up codebase

--- main.py
+++ main.py
@@ -5,31 +5,52 @@ from google import genai
 from google.genai import types
 
 def main():
-    model = "gemini-2.0-flash-001"
-    #prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
-    
     load_dotenv()
-    api_key = os.environ.get("GEMINI_API_KEY")
     
+    verbose = "--verbose" in sys.argv
+    args = []
+    for arg in sys.argv[1:]:
+        if not arg.startswith("--"):
+            args.append(arg)
+    
+    if not args:
+        print("Aero AI Code Assistant")
+        print('\nUsage: python main.py "your prompt here" [--verbose]')
+        print('Example: python main.py "How do I build a calculator app?"')
+        sys.exit(1)
+        
+    api_key = os.environ.get("GEMINI_API_KEY")
     client = genai.Client(api_key=api_key)
     
-    if len(sys.argv) == 0:
-        sys.exit(1)
-    user_prompt = sys.argv[1]
+    user_prompt = " ".join(args)
+    
+    if verbose:
+        print(f"User prompt: {user_prompt}\n")
+    
     
     messages = [
         types.Content(role="user", parts=[types.Part(text=user_prompt)]),
     ]
     
-    response = client.models.generate_content(
-        model=model, contents=messages
-    )
+    generate_content(client, messages, verbose)
     
+def generate_content(client, messages, verbose):
+    response = client.models.generate_content(
+        model="gemini-2.0-flash-001",
+        contents=messages
+    )
+
+    if verbose:
+        prompt_tokens = response.usage_metadata.prompt_token_count
+        resp_tokens = response.usage_metadata.candidates_token_count
+        print(f"Prompt tokens: {prompt_tokens}")
+        print(f"Response tokens: {resp_tokens}")
+    
+    print("Response:")
     print(response.text)
-    prompt_tokens = response.usage_metadata.prompt_token_count
-    resp_tokens = response.usage_metadata.candidates_token_count
-    print(f"Prompt tokens: {prompt_tokens}")
-    print(f"Response tokens: ")
+    
+
+
 
 
 
