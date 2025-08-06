e6339ea7cb8d26f3d46ecb6e703b98db7ef9550c
Aerospace91 <beenters@gmail.com>
2025-08-06 12:56:31 -0700

    Basic Prompt and Response

--- main.py
+++ main.py
@@ -1,7 +1,19 @@
+import os
+from dotenv import load_dotenv
+from google import genai
+
 def main():
-    print("Hello from AeroAI")
-
-
+    model = "gemini-2.0-flash-001"
+    prompt = "Why is Boot.dev such a great place to learn backend development? Use one paragraph maximum."
+    load_dotenv()
+    api_key = os.environ.get("GEMINI_API_KEY")
+    client = genai.Client(api_key=api_key)
+    resp = client.models.generate_content(model=model, contents=prompt)
+    print(resp.text)
+    prompt_tokens = resp.usage_metadata.prompt_token_count
+    resp_tokens = resp.usage_metadata.candidates_token_count
+    print(f"Prompt tokens: {prompt_tokens}")
+    print(f"Response tokens: ")
 
 
 
