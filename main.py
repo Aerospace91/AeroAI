4110f24df8360f16348a319a835d448837cc3767
Aerospace91 <beenters@gmail.com>
2025-08-06 14:13:41 -0700

    Fixing a bit of formatting

--- main.py
+++ main.py
@@ -2,6 +2,7 @@ import os
 import sys
 from dotenv import load_dotenv
 from google import genai
+from google.genai import types
 
 def main():
     model = "gemini-2.0-flash-001"
@@ -14,12 +15,19 @@ def main():
     
     if len(sys.argv) == 0:
         sys.exit(1)
-    prompt = sys.argv[1]
-    resp = client.models.generate_content(model=model, contents=prompt)
+    user_prompt = sys.argv[1]
     
-    print(resp.text)
-    prompt_tokens = resp.usage_metadata.prompt_token_count
-    resp_tokens = resp.usage_metadata.candidates_token_count
+    messages = [
+        types.Content(role="user", parts=[types.Part(text=user_prompt)]),
+    ]
+    
+    response = client.models.generate_content(
+        model=model, contents=messages
+    )
+    
+    print(response.text)
+    prompt_tokens = response.usage_metadata.prompt_token_count
+    resp_tokens = response.usage_metadata.candidates_token_count
     print(f"Prompt tokens: {prompt_tokens}")
     print(f"Response tokens: ")
 
